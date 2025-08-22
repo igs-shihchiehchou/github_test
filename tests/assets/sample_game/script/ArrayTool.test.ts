02478f93649c18f332cbe3c21552040d116cfca2
shihchiehchou <shihchiehchou@igs.com.tw>
2025-08-22 17:43:43 +0800

    feat: remove unit test to demo a failed coverage pr

--- tests/assets/sample_game/script/ArrayTool.test.ts
+++ tests/assets/sample_game/script/ArrayTool.test.ts
@@ -6,12 +6,6 @@ jest.mock('cc', () => ({
     Vec3: jest.fn().mockImplementation((x, y, z) => ({ x, y, z })),
 }));
 
-test('test fastRemoveArrayItemAt should return correct boolean', () => {
-    expect(ArrayTool.fastRemoveArrayItemAt([1,2,3], 0)).toBe(true);
-    expect(ArrayTool.fastRemoveArrayItemAt([1,2,3], 4)).toBe(false);
-    expect(ArrayTool.fastRemoveArrayItemAt([1,2,3], -1)).toBe(false);
-});
-
 test('test getX should return correct x value from Vec3', () => {
     const vec3 = new Vec3(1, 2, 3);
     expect(ArrayTool.getX(vec3)).toBe(1);
